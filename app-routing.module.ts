e0d0a1be254a16937c9b1656bd7c160193d234a2
bastianalexander22 <83126237+bastianalexander22@users.noreply.github.com>
2023-09-02 13:53:07 -0400

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'acceso',
+    loadChildren: () => import('./acceso/acceso.module').then( m => m.AccesoPageModule)
+  },
+
+  {
+    path: 'pagina-acceso',
+    loadChildren: () => import('./acceso/acceso.module').then(m => m.AccesoPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
